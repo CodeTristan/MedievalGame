5fb97423ccdad44ed56ec106e517ed090491d574
CodeTristan <5aerenmertozen@gmail.com>
2023-01-27 00:37:24 +0300

    Dialog System 1

--- MedievalGameRealOne/Assets/Dialog.cs
+++ MedievalGameRealOne/Assets/Dialog.cs
@@ -3,21 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class DialogSprite
-{
-    public string assetName;
-    public Sprite sprite;
-}
-[System.Serializable]
 public class Dialog
 {
     public string name;
     public List<string> sentences;
-    public DialogSprite dialogSprite;
+    public Sprite dialogSprite;
+    public Sprite fullScreenImageSprite;
     public Dialog()
     {
         sentences = new List<string>();
-        dialogSprite = new DialogSprite();
     }
 
 }
--- MedievalGameRealOne/Assets/DialogManager.cs
+++ MedievalGameRealOne/Assets/DialogManager.cs
@@ -7,17 +7,22 @@ using System;
 
 public class DialogManager : MonoBehaviour
 {
-    public DialogSprite[] AllSprites;
+    public Sprite[] AllSprites;
+    public string[] AllDialogPaths;
+    public Sprite[] AllFullScreenImages;
     public List<Dialog> dialogs;
 
     private StreamReader reader;
-    string path = "Assets/text.txt";
     private void Start()
     {
-
-        //Start Dialog Part
         dialogs = new List<Dialog>();
+        StartDialog(FindPath("text"));
+    }
 
+    public void StartDialog(string path)
+    {
+
+        //Tries to open file in given path.
         try
         {
             reader = new StreamReader(path, Encoding.GetEncoding("ISO-8859-9"));
@@ -29,6 +34,7 @@ public class DialogManager : MonoBehaviour
         }
 
         Dialog tempDialog = new Dialog();
+
         while (reader.EndOfStream == false)
         {
             if (reader.Peek() == '<')
@@ -36,7 +42,12 @@ public class DialogManager : MonoBehaviour
                 //This char is for spacing and it means skip this 
                 reader.ReadLine();
             }
-            if (reader.Peek() == '-')
+            //This means its the start of dialog. After the - comes name of the speaker.
+            // - means speaker's name
+            // * means speaker's sprite
+            // # means sound effect to play
+            // $ means full screen picture to show
+            if (reader.Peek() == '-') 
             {
                 if (tempDialog.sentences.Count > 0)
                 {
@@ -44,13 +55,38 @@ public class DialogManager : MonoBehaviour
                 }
                 tempDialog = new Dialog();
 
+                //Speaker's name
                 reader.Read(); // Skips '-' char
                 tempDialog.name = reader.ReadLine();   //Reads name
 
-                reader.Read(); // Skips '*' char
-                string spriteName = reader.ReadLine();
-                tempDialog.dialogSprite.sprite = FindSprite(spriteName);  //Reads spriteName and sends it to FindSprite function.
-                tempDialog.dialogSprite.assetName = spriteName;
+                //Speaker's sprite
+                if(reader.Peek() == '*')
+                {
+                    reader.Read(); // Skips '*' char
+                    tempDialog.dialogSprite = FindSprite(reader.ReadLine());  //Reads spriteName and sends it to FindSprite function.
+                }
+                else
+                {
+                    tempDialog.dialogSprite = null;
+                }
+                
+
+                //Sound Effect
+                //Add sound effect thing when sound manager is ready
+                //Add sound material to dialog
+
+                //Full screen Image to play
+                if(reader.Peek() == '$')
+                {
+                    reader.Read(); // Skips '$' char
+                    tempDialog.fullScreenImageSprite = FindFullScreenImageSprite(reader.ReadLine());
+                }
+                else
+                {
+                    tempDialog.fullScreenImageSprite = null;
+                }
+                
+
             }
             else
             {
@@ -59,11 +95,18 @@ public class DialogManager : MonoBehaviour
 
         }
 
+        //Debugs
+
         Debug.Log(dialogs.Count);
 
         for (int i = 0; i < dialogs.Count; i++)
         {
-            Debug.Log("Name: " + dialogs[i].name + "  Sprite: " + dialogs[i].dialogSprite.assetName);
+            Debug.Log("Name: " + dialogs[i].name);
+
+            if (dialogs[i].dialogSprite != null)
+                Debug.Log("Sprite: " + dialogs[i].dialogSprite.name);
+            else
+                Debug.Log("Sprite: null");
 
             for (int j = 0; j < dialogs[i].sentences.Count; j++)
             {
@@ -73,22 +116,58 @@ public class DialogManager : MonoBehaviour
             Debug.Log("-------");
         }
 
-
+        //Closes the StreamReader
         reader.Close();
+
+        //TO-DO: Connect UI with code.
+
     }
 
     // Finds and returns sprite in AllSprites array according to given string spriteName
     private Sprite FindSprite(string spriteName)
     {
+        if (spriteName == null)  //This is end indicator. This doesnt effect dialogs so in order to prevent unneccesary error log. It returns here.
+            return null;
+
         for (int i = 0; i < AllSprites.Length; i++)
         {
-            if (spriteName == AllSprites[i].assetName)
+            if (spriteName == AllSprites[i].name)
             {
-                return AllSprites[i].sprite;
+                return AllSprites[i]    ;
             }
         }
 
         Debug.Log("ERROR: " + spriteName + " named sprite couldn't found.");
+        return null;
+    }
+
+    //Finds the given text file's path in assets.
+    private string FindPath(string name)
+    {
+        for (int i = 0; i < AllDialogPaths.Length; i++)
+        {
+            if(AllDialogPaths[i] == name)
+            {
+                return "Assets/Dialogs/" + AllDialogPaths[i] + ".txt";
+            }
+        }
+
+        //If file path couldn't found returns error string
+        return "NoPathErrorInFindPathFunction";
+    }
+
+    //Finds the given named sprite in AllFullScreenImages array
+    private Sprite FindFullScreenImageSprite(string spriteName)
+    {
+        for (int i = 0; i < AllFullScreenImages.Length; i++)
+        {
+            if(AllFullScreenImages[i].name == spriteName)
+            {
+                return AllFullScreenImages[i];
+            }
+        }
+
+        //If none sprite is found. It returns null
         return null;
     }
 }
